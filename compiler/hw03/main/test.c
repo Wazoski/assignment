5ac0dab023a37f5e9e9eab1d478b49c7b74220f9
beyondbestas <135kkkiii@naver.com>
2015-11-17 20:05:34 +0900

    new

--- compiler/hw03/main/test.c
+++ compiler/hw03/main/test.c
@@ -1,16 +1,1 @@
 int func1 (int a,int b){
-    int result;
-    result = 0;
-    while (a> 0){if (a/ b )
-        {result=result+1;}
-        a =a%b;
-    }
-    return result;
-}
-
-int main(void){
-    int a;int b;int result;
-    a=256;b=2;
-    result= func1 (a,   b);
-    return 0;
-}
